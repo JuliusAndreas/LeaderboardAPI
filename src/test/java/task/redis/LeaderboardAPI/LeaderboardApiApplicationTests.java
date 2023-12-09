030f38d883ae0aa56b2e8cd4741f06e26ee19f3e
Soroosh Faal <faalsoroosh@gmail.com>
2023-12-09 13:04:48 +0330

    add project files

---
+++ src/test/java/task/redis/LeaderboardAPI/LeaderboardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package task.redis.LeaderboardAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaderboardApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
