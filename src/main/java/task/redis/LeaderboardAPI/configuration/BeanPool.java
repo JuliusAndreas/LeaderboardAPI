6cb9b5e8e702fbc8a9b9df677d6d8b744bc00b36
Soroosh Faal <faalsoroosh@gmail.com>
2023-12-16 12:22:17 +0330

    add redis config

---
+++ src/main/java/task/redis/LeaderboardAPI/configuration/BeanPool.java
@@ -0,0 +1,24 @@
+package task.redis.LeaderboardAPI.configuration;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.ClusterServersConfig;
+import org.redisson.config.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+//@Data
+@Configuration
+@RequiredArgsConstructor
+public class BeanPool {
+    private final RedisConfig redisConfig;
+
+    @Bean
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        config.useSingleServer().setAddress(redisConfig.getHost().concat(redisConfig.getPort()));
+        return Redisson.create(config);
+    }
+}
