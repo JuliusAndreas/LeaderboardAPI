a9cb20bd32edf64bc3b5485d3bbd9f95c5ffd3bb
Soroosh Faal <faalsoroosh@gmail.com>
2023-12-18 11:34:20 +0330

    writing the leaderboard to the output file

--- src/main/java/task/redis/LeaderboardAPI/service/ActivityService.java
+++ src/main/java/task/redis/LeaderboardAPI/service/ActivityService.java
@@ -2,30 +2,39 @@ package task.redis.LeaderboardAPI.service;
 
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.redisson.api.LocalCachedMapOptions;
 import org.redisson.api.RMap;
 import org.redisson.api.RedissonClient;
 import org.redisson.codec.TypedJsonJacksonCodec;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import task.redis.LeaderboardAPI.util.AnswerStatus;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ActivityService {
     private final RedissonClient redis;
     private RMap<String, Integer> leaderboard;
     public String leaderboardKey = "leaderboard";
+    final static String outputFilePath
+            = "./leaderboard.txt";
 
     @PostConstruct
     public void init() {
-        LocalCachedMapOptions options = LocalCachedMapOptions.defaults()
+        LocalCachedMapOptions<Object, Object> options = LocalCachedMapOptions.defaults()
                 .cacheSize(0)
                 .evictionPolicy(LocalCachedMapOptions.EvictionPolicy.LRU)
                 .maxIdle(10, TimeUnit.SECONDS)
                 .timeToLive(60, TimeUnit.SECONDS);
-        leaderboard = redis.getLocalCachedMap(leaderboardKey, new TypedJsonJacksonCodec(String.class) ,options);
+        leaderboard = redis.getMapCache(leaderboardKey, new TypedJsonJacksonCodec(String.class));
         leaderboard.clear();
     }
 
@@ -36,4 +45,25 @@ public class ActivityService {
             leaderboard.put(username, answerStatus == AnswerStatus.CORRECT ? 1 : -1);
         }
     }
+
+    @Scheduled(initialDelay = 30, fixedDelay = 60, timeUnit = TimeUnit.SECONDS)
+    public void releaseLock() {
+
+    }
+
+    @Scheduled(fixedDelay = 60, timeUnit = TimeUnit.SECONDS)
+    public void writeToFile() {
+        File file = new File(outputFilePath);
+        try (BufferedWriter bf = new BufferedWriter(new FileWriter(file))) {
+            for (RMap.Entry<String, Integer> entry : leaderboard.entrySet()) {
+                bf.write(entry.getKey() + " : " + entry.getValue());
+                bf.newLine();
+            }
+            log.info("successfully wrote {} records to the output file", leaderboard.size());
+            bf.flush();
+        } catch (IOException e) {
+            log.error("Error encountered while trying to write to the file", e);
+        }
+    }
+
 }
